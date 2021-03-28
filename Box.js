bc54136a5b54b711ab2eefb681a667255fab011e
VivaanVerma2347 <79014122+VivaanVerma2347@users.noreply.github.com>
2021-03-28 13:10:33 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,24 @@
+class Box{
+ constructor(x,y,width,height){
+    var box_options ={
+        restitution: 1.0
+    }
+
+    this.body = Bodies.rectangle(x,y,width,height,box_options);
+    World.add(world,this.body);
+    this.width=width;
+    this.height=height;   
+ }   
+
+display(){
+    var angle=this.body.angle;
+    push();
+    translate (this.body.position.x,this.body.position.y);
+    rotate(angle)
+    rectMode(CENTER);
+    fill ("blue");
+    rect(0,0,this.width,this.height);
+    pop();
+}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class-23-oops
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
